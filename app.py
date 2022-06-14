9ff5d838db29320a6df9a74b2f00282d0cad4715
HamDev235 <102918374+HamDev235@users.noreply.github.com>
2022-06-13 22:50:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # My first app
@@ -6,3 +7,6 @@ Hello *world!*
 """)
 
 slider = st.slider("This is a slider.")
+df = pd.read_csv('https://nightly.omniscope.me/Richard/timeSeries.csv?download')
+
+st.dataframe(df)  # Same as st.write(df)
